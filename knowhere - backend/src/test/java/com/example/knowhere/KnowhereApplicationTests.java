7bb865cc2a24987abb48a164ba77aa9d89c0b8f3
Andrea <andrea.griguoli@edu.unito.it>
2025-04-23 11:11:16 +0200

    last commit

---
+++ knowhere - backend/src/test/java/com/example/knowhere/KnowhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowhere;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KnowhereApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
